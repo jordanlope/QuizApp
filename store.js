923ed55b63d8ea5fe9f94bf945aa59e22a4dbc5a
Jordan Lopez <jordanlopez992@gmail.com>
2020-01-21 15:04:40 -0500

    Added Store data

---
+++ store.js
@@ -0,0 +1,56 @@
+const STORE = {
+    questions: [
+        {
+            question: 'Who was the only president unanimously elected? Meaning all of the state representatives voted for him.',
+            choices: [
+                'Abraham Lincoln',
+                'George W Bush',
+                'George Washington',
+                'Theodore Roosevelt',
+            ],
+            correctAnswer: 'George Washington'
+        },
+        {
+            question: 'Who often stored things like letters and documents in his tall stove-piped hat?',
+            choices: [
+                'Abraham Lincoln',
+                'Theodore Roosevelt',
+                'Warren Harding',
+                'William Howard Taft',
+            ],
+            correctAnswer: 'Abraham Lincoln'
+        },
+        {
+            question: 'Who was the first president who was a Boy Scout?',
+            choices: [
+                'Grover Cleveland',
+                'Donald Trump',
+                'John F. Kennedy',
+                'Richard Nixon',
+            ],
+            correctAnswer: 'John F. Kennedy'
+        },
+        {
+            question: 'Who won a Grammy Award in 2006 for his voice on the audiobook Dreams From My Father?',
+            choices: [
+                'Jimmy Carter',
+                'Gerald Ford',
+                'John F. Kennedy',
+                'Barack Obama',
+            ],
+            correctAnswer: 'Barack Obama'
+        }, 
+        {
+            question: 'Which president had 15 children? The White House must have been hopping!',
+            choices: [
+                'Franklin Pierce',
+                'John Tyler',
+                'Benjamin Harrison',
+                'Woodrow Wilson',
+            ],
+            correctAnswer: 'John Tyler'
+        }, 
+    ],
+    score: 0,
+    questionIndex: 0,
+}
