61095d20a76f407a38c5121241bac3749cad41ae
Jordan Lopez <jordanlopez992@gmail.com>
2020-01-22 19:20:39 -0500

    Completed All Functionality Requirements of Quiz App

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 function startQuiz() {
+    STORE.questionIndex = 0;
+    STORE.score = 0;
     $('#begin-quiz button').click(function() {
         const parentID = $(this).closest('section').attr('id');
         console.log(parentID);
@@ -6,15 +8,85 @@ function startQuiz() {
     });
 }
 
-function nextQuestion() {
-    $('body').on("click", 'form #submit-button', function(event) {
+function evalQuestion() {
+    $('body').on("click", 'form #js-submit-button', function(event) {
             event.preventDefault();
-            console.log('Form button was pressed');
+
             const parentID = $(this).closest('section').attr('id');
-            STORE.questionIndex += 1;
-            renderQuestions(parentID);
-        }
-    )
+            const selAnswer = $(this).closest('section').find('.checked').attr('value');
+
+            if (!selAnswer) {
+                return promptSelection('No selection!');
+            } else if(checkAnswer(selAnswer) == 'correct') {
+                // Give user feedback
+                STORE.questionIndex += 1;
+                STORE.score += 1;
+                promptSelection('Correct!');
+                nextButton();
+            } else if(checkAnswer(selAnswer) == 'inCorrect') {
+                // Give user feedback
+                STORE.questionIndex += 1;
+                promptSelection('incorrect!');
+                nextButton();
+            }
+    })
+}
+
+function selectedOption() {
+    $('body').on('click','.js-form-option', event => {
+        console.log('Hi');
+        const targetAnswer = $(event.currentTarget);
+
+        const otherAnswers = $('.js-form-option').not(targetAnswer);
+
+        const pressedBool = $(targetAnswer).attr('aria-pressed') === 'true';
+
+        otherAnswers.removeClass('checked').attr('aria-pressed', false);
+
+        targetAnswer.addClass('checked').attr('aria-pressed', !pressedBool);
+    })
+}
+
+function whenNextButtonClicked() {
+    $('body').on("click",'form #js-next-button', function(event) {
+        event.preventDefault();
+        const parentID = $(this).closest('section').attr('id');
+        console.log(parentID);
+        renderQuestions(parentID);
+    })
+}
+
+function tryAgain() {
+    $('body').on("click", '#results-quiz button', function(event) {
+        const parentID = $(this).closest('section').attr('id');
+        STORE.questionIndex = 0;
+        STORE.score = 0;
+        renderQuestions(parentID);
+    });
+}
+
+function promptSelection(displayText) {
+    const feedback = $('form').closest('form').find('#feedback');
+    console.log(feedback);
+    if(!feedback) {
+        $('form').after('<p id="feedback">' + displayText + '</p>');
+    } else {
+        $('form').find('#feedback').replaceWith('<p id="feedback">' + displayText + '</p>');
+    }
+}
+
+function checkAnswer(answer) {
+    if(answer == STORE.questions[STORE.questionIndex].correctAnswer) {
+        return 'correct';
+    } else if(answer != STORE.questions[STORE.questionIndex].correctAnswer) {
+        return 'inCorrect';
+    } else {
+        return 'noAnswer'
+    }
+}
+
+function nextButton() {
+    $('form #js-submit-button').replaceWith("<input type='submit' id='js-next-button' class='main-button' value='Next'></input>");
 }
 
 function renderQuestion(index, parentId) {
@@ -25,11 +97,12 @@ function renderQuestion(index, parentId) {
             "<p id='question-num'>" + "QUESTION:" + (questionIndex) +"</p>" +
             "<h2>" + STORE.questions[index].question + "</h2>" +
             "<form onsubmit='return false;' class='js-quiz-form'>" +
-                "<input type='button' name='answer' value='" + STORE.questions[index].choices[0] + "' class='button'>" +
-                "<input type='button' name='answer' value='" + STORE.questions[index].choices[1] + "' class='button'>" +
-                "<input type='button' name='answer' value='" + STORE.questions[index].choices[2] + "' class='button'>" +
-                "<input type='button' name='answer' value='" + STORE.questions[index].choices[3] + "' class='button'>" +
-                "<input type='submit' id='submit-button' value='Submit'>" +
+                "<input type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[0] + "'>" +
+                "<input type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[1] + "'>" +
+                "<input type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[2] + "'>" +
+                "<input type='button' aria-pressed='false' class='form-option js-form-option' name='answer' value='" + STORE.questions[index].choices[3] + "'>" +
+                "<input type='submit' id='js-submit-button' class='main-button' value='Submit'>" +
+                "<p id='feedback'></p>" +
             "</form>" +
         "</section>"
     );
@@ -39,15 +112,15 @@ function renderResults(parentID) {
     $('#' + parentID).replaceWith(
         '<section id="results-quiz">' +
             '<h2>Quiz Results</h2>' +
-            '<p id="results">Results: 4/5</p>' +
-            '<button type="button">Try Again</button>' +
+            '<p id="results">Results:'+ STORE.score + '/' + STORE.questions.length + '</p>' +
+            '<button class="main-button" type="button">Try Again</button>' +
         '</section>'
     )
 }
 
 function renderQuestions(parentID) {
     const index = STORE.questionIndex;
-    if(STORE.questionIndex == 5) {
+    if(STORE.questionIndex == STORE.questions.length) {
         renderResults(parentID);
     } else {
         renderQuestion(index, parentID);
@@ -55,14 +128,12 @@ function renderQuestions(parentID) {
 }
 
 
-function results() {
-    console.log('results')
-}
-
 function handleQuizApp() {
     startQuiz();
-    nextQuestion();
-    results();
+    selectedOption();
+    evalQuestion();
+    tryAgain();
+    whenNextButtonClicked();
 }
 
 $(handleQuizApp)
